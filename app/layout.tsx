5a1c2c3f5d5b90d378f900293fc5f8467360cd08
GINTOAHC <chaotingchen10@gmail.com>
2024-03-17 00:42:47 +0800

    fix: suppress next-theme warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-screen">
+    <html lang="en" className="h-screen" suppressHydrationWarning>
       <body className="flex flex-row h-full">
         <ThemeProvider>
           <Providers>
